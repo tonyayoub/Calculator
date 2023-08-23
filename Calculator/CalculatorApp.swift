0afd2e17294c189d21fbdc738d46e09414c3e520
Tony Ayoub <tonysameh@hotmail.com>
2023-08-23 09:52:53 +0200

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Tony Ayoub on 23-08-2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
