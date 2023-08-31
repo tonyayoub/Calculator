3ae6ce4db7bb0a7d9cfa8731b82ae36309a7508f
Tony Ayoub <tonysameh@hotmail.com>
2023-08-31 12:05:30 +0200

    Display correct Bitcoing value + Dark mode

---
+++ Calculator/UI/Settings/DarkModeSwitch.swift
@@ -0,0 +1,18 @@
+//
+//  DarkModeSwitch.swift
+//  Calculator
+//
+//  Created by Tony Ayoub on 31-08-2023.
+//
+
+import SwiftUI
+
+struct DarkModeSwitch: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DarkModeSwitch()
+}
